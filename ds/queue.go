4e9e79a6d0427373d467c9ffcb52bc12334abb7e
Joshua-Vann <jvann2@uwyo.edu>
2026-02-04 20:24:45 -0700

    Update queue.go

---
+++ ds/queue.go
@@ -0,0 +1,26 @@
+//Joshua Vann
+//COSC 3750
+//2-1-26
+
+package ds
+
+import "errors"
+
+type Queue struct {
+	list LinkedList
+}
+
+// add tail node
+func (q *Queue) Push(value string) {
+	q.list.RemoveAt(q.list.GetSize() - 1)
+}
+
+// remove the head
+func (q *Queue) Pop() (string, error) {
+	if q.list.Size == 0 {
+		return "", errors.New("Cannot perform on empty Queue")
+	}
+	r := q.list.Head.data
+	q.list.RemoveAt(0)
+	return r, nil
+}
